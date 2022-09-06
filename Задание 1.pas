52c484a2120f36c2b4050466af2687e050fe31ab
Natusik34 <112948065+Natusik34@users.noreply.github.com>
2022-09-06 21:52:40 +0300

    Add files via upload

---
+++ Задание 1.pas
@@ -0,0 +1,26 @@
+﻿Program abc;
+const N = 3;
+var 
+  ai: array[1..N] of string;
+  a, b: integer;
+  c: boolean;
+  begin
+c:= false;
+for a:=1 to N do
+begin
+   writeln('Введите номерной знак ',a);
+ readln(ai[a]);
+ end;
+writeln('---------');
+b := 0;
+for a:=1 to N do
+ begin
+ b := pos('MOH',ai[a]);
+ if b <> 0 then begin
+                writeln(ai[a]);
+                if not c then c := true;
+                end;
+ end;
+ if not c then writeln('Такого знака нет');
+readln;
+end.
---
+++ Задание 2.pas
@@ -0,0 +1,13 @@
+﻿Program primer;
+var
+  a,b,s,x,k: integer;
+  begin
+    x:=3;
+    k:=2;
+    a:= sqr(1-2)*(1-2)+sqr(2-2)*(2-2)+sqr(3-2)*(3-2)+sqr(4-2)*(4-2)+sqr(5-2)*(5-2)+sqr(6-2)*(6-2)+sqr(7-2)*(7-2)+sqr(8-2)*(8-2)+sqr(9-2)*(9-2)+sqr(10-2)*(10-2);
+   // b:= 1*sqr(k)+2*sqr(k)+3*sqr(k)+4*sqr(k)+5*sqr(k)+6*sqr(k)+7*sqr(k)+8*sqr(k)+9*sqr(k)+10*sqr(k);
+   b:= sqr(1)+sqr(2)+sqr(3)+sqr(4)+sqr(5)+sqr(6)+sqr(7)+sqr(8)+sqr(9)+sqr(10);
+    s:= a+b;
+    writeln(s);
+    readln;
+  end.
